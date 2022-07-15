f4b7ba49910aeb00d72ed3e7a4032e9d22e4aaa4
Thedogecraft <97050087+Thedogecraft@users.noreply.github.com>
2022-07-15 14:34:47 -0700

    Code.bat

---
+++ Code.bat
@@ -0,0 +1,12 @@
+ @echo off
+Cls
+Title Parcoil PC Cleaner v1.0.5
+Echo WARNING: this file will delete files if you contiue
+Pause
+Cd /d %temp%
+Del *.*
+Cd low
+Del *.*
+
+Echo complete. please email us if you have suggestions or Have Issues
+Pause
